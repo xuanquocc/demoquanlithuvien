8539d0ead854ab2d7fe73f4351dd0b0f7a17610a
xuanquoc <nguyentrinhxuanquoc2003@gmail.com>
2022-04-07 11:59:29 +0700

    Init project

---
+++ assets/js/book.js
@@ -0,0 +1,9 @@
+function add(){
+    let ID = document.getElementById('id').value;
+    let name = document.getElementById('name').value;
+    let composer = document.getElementById('composer').value;
+    let publishingcompany = document.getElementById('publishingcompany').value;
+    let type = document.getElementById('type').value;
+
+    console.log(ID, name, composer, publishingcompany, type);
+}
